e8c11869455d1c75a33402cd2f92bd5f70f1fb18
Alfonso Landeros <alanderos@ucla.edu>
2021-09-07 09:23:52 -0700

    Add utilities from previous project

---
+++ src/projections.jl
@@ -0,0 +1,76 @@
+"""
+Project `x` onto sparsity set with `k` non-zero elements.
+Assumes `idx` enters as a vector of indices into `x`.
+"""
+function project_l0_ball!(x, idx, k)
+    #
+    #   do nothing if k > length(x)
+    #
+    if k ≥ length(x) return x end
+    
+    #
+    #   fill with zeros if k ≤ 0
+    #
+    if k ≤ 0 return fill!(x, 0) end
+    
+    #
+    # find the spliting element
+    #
+    pivot = l0_search_partialsort!(idx, x, k)
+    
+    #
+    # apply the projection
+    #
+    kcount = 0
+    @inbounds for i in eachindex(x)
+        if abs(x[i]) <= abs(pivot) || kcount ≥ k
+            x[i] = 0
+        else
+            kcount += 1
+        end
+    end
+    
+    return x
+end
+
+"""
+Search `x` for the pivot that splits the vector into the `k`-largest elements in magnitude.
+
+The search preserves signs and returns `x[k]` after partially sorting `x`.
+"""
+function l0_search_partialsort!(idx, x, k)
+    #
+    # Based on https://github.com/JuliaLang/julia/blob/788b2c77c10c2160f4794a4d4b6b81a95a90940c/base/sort.jl#L863
+    # This eliminates a mysterious allocation of ~48 bytes per call for
+    #   sortperm!(idx, x, alg=algorithm, lt=isless, by=abs, rev=true, initialized=false)
+    # where algorithm = PartialQuickSort(lo:hi)
+    # Savings are small in terms of performance but add up for CV code.
+    #
+    lo = k
+    hi = k+1
+
+    # Order arguments
+    lt  = isless
+    by  = abs
+    rev = true
+    o = Base.Order.Forward
+    order = Base.Order.Perm(Base.Sort.ord(lt, by, rev, o), x)
+
+    # Initialize the idx array; algorithm relies on idx[i] = i
+    @inbounds for i in eachindex(idx)
+        idx[i] = i
+    end
+
+    # sort!(idx, lo, hi, PartialQuickSort(k), order)
+    Base.Sort.Float.fpsort!(idx, PartialQuickSort(lo:hi), order)
+    
+    return x[idx[k+1]]
+end
+
+struct ApplyL0Projection <: Function
+    idx::Vector{Int}
+end
+
+function (P::ApplyL0Projection)(x, k)
+    project_l0_ball!(x, P.idx, k)
+end
---
+++ src/utilities.jl
@@ -0,0 +1,42 @@
+"""
+Apply acceleration to the current iterate `x` based on the previous iterate `y`
+according to Nesterov's method with parameter `r=3` (default).
+"""
+function __apply_nesterov__!(x, y, iter::Integer, needs_reset::Bool, r::Int=3)
+    if needs_reset # Reset acceleration scheme
+        copyto!(y, x)
+        iter = 1
+    else # Nesterov acceleration 
+        γ = (iter - 1) / (iter + r - 1)
+        @inbounds @simd for i in eachindex(x)
+            xi, yi = x[i], y[i]
+            zi = xi + γ * (xi - yi)
+            x[i], y[i] = zi, xi
+        end
+        iter += 1
+    end
+
+    return iter
+end
+
+"""
+Map a sparsity level `s` to an integer `k`, assuming `n` elements.
+"""
+sparsity_to_k(s, n) = round(Int, n * (1-s))
+
+"""
+Define a geometric progression recursively by the rule
+```
+    rho_new = min(rho_max, rho * multiplier)
+```
+The result is guaranteed to have type `typeof(rho)`.
+"""
+function geometric_progression(rho, iter, rho_max, multiplier::Real=1.2)
+    return convert(typeof(rho), min(rho_max, rho * multiplier))
+end
+
+"""
+Placeholder for callbacks in main functions.
+"""
+__do_nothing_callback__(iter, problem, lambda, rho, k, history) = nothing
+__do_nothing_callback__(fold, problem, train_problem, data, lambda, sparsity, model_size, result) = nothing
