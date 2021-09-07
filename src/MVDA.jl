4f63cc6f35c6764e9b476af16fb18c8eedabe0f7
Alfonso Landeros <alanderos@ucla.edu>
2021-09-07 08:44:15 -0700

    Add datasets using DataDeps

--- src/MVDA.jl
+++ src/MVDA.jl
@@ -1,5 +1,89 @@
 module MVDA
 
-# Write your package code here.
+using DataFrames: copy, copyto!
+using DataDeps, CSV, DataFrames, CodecZlib
+using LinearAlgebra, Random, Statistics, StableRNGs
+
+##### DATA #####
+
+#=
+Uses DataDeps to download data as needed.
+Inspired by UCIData.jl: https://github.com/JackDunnNZ/UCIData.jl
+=#
+
+const DATA_DIR = joinpath(@__DIR__, "data")
+
+"""
+`list_datasets()`
+
+List available datasets in SparseSVM.
+"""
+list_datasets() = map(x -> splitext(x)[1], readdir(DATA_DIR))
+
+function __init__()
+    for dataset in list_datasets()
+        include(joinpath(DATA_DIR, dataset * ".jl"))
+    end
+end
+
+"""
+`dataset(str)`
+
+Load a dataset named `str`, if available. Returns data as a `DataFrame` where
+the first column contains labels/targets and the remaining columns correspond to
+distinct features.
+"""
+function dataset(str)
+    # Locate dataset file.
+    dataset_path = @datadep_str str
+    file = readdir(dataset_path)
+    index = findfirst(x -> occursin("data.", x), file)
+    if index isa Int
+        dataset_file = joinpath(dataset_path, file[index])
+    else # is this unreachable?
+        error("Failed to locate a data.* file in $(dataset_path)")
+    end
+    
+    # Read dataset file as a DataFrame.
+    df = if splitext(dataset_file)[2] == ".csv"
+        CSV.read(dataset_file, DataFrame)
+    else # assume .csv.gz
+        open(GzipDecompressorStream, dataset_file, "r") do stream
+            CSV.read(stream, DataFrame)
+        end
+    end
+    return df
+end
+
+function process_dataset(path::AbstractString; header=false, missingstrings="", kwargs...)
+    input_df = CSV.read(path, DataFrame, header=header, missingstrings=missingstrings)
+    process_dataset(input_df; kwargs...)
+    rm(path)
+end
+
+function process_dataset(input_df::DataFrame;
+    target_index=-1,
+    feature_indices=1:0,
+    ext=".csv")
+    # Build output DataFrame.
+    output_df = DataFrame()
+    output_df.target = input_df[!, target_index]
+    output_df = hcat(output_df, input_df[!, feature_indices], makeunique=true)
+    output_cols = [ :target; [Symbol("x", n) for n in eachindex(feature_indices)] ]
+    rename!(output_df, output_cols)
+    dropmissing!(output_df)
+    
+    # Write to disk.
+    output_path = "data" * ext
+    if ext == ".csv"
+        CSV.write(output_path, output_df, delim=',', writeheader=true)
+    elseif ext == ".csv.gz"
+        open(GzipCompressorStream, output_path, "w") do stream
+            CSV.write(stream, output_df, delim=",", writeheader=true)
+        end
+    else
+        error("Unknown file extension option '$(ext)'")
+    end
+end
 
 end
