e04a6d21bc4b98b4802383ad00bb0249044b7f7c
Alfonso Landeros <alanderos@ucla.edu>
2021-11-18 12:48:27 -0800

    Clean up documentation and rename a few functions; WIP

--- src/MVDA.jl
+++ src/MVDA.jl
@@ -111,14 +111,6 @@ include("simulation.jl")
 
 abstract type AbstractMMAlg end
 
-__mm_init__(algorithm::AbstractMMAlg, problem, extras) = not_implemented(algorithm, "Initialization step")
-__mm_iterate__(algorithm::AbstractMMAlg, problem, ϵ, ρ, k, extras) = not_implemented(algorithm, "Iteration step")
-__mm_iterate__(algorithm::AbstractMMAlg, problem, ϵ, λ, extras) = not_implemented(algorithm, "Iteration step (regularized)")
-__mm_iterate__(algorithm, problem, ϵ, δ, λ₁, λ₂, extras) = not_implemented(algorithm, "Iteration step (Euclidean)")
-__mm_update_sparsity__(algorithm::AbstractMMAlg, problem, ϵ, ρ, k, extras) = not_implemented(algorithm, "Update sparsity step")
-__mm_update_rho__(algorithm::AbstractMMAlg, problem, ϵ, ρ, k, extras) = not_implemented(algorithm, "Update ρ step")
-__mm_update_lambda__(algorithm::AbstractMMAlg, problem, ϵ, λ, extras) = not_implemented(algorithm, "Update λ step")
-
 include(joinpath("algorithms", "SD.jl"))
 include(joinpath("algorithms", "MMSVD.jl"))
 include(joinpath("algorithms", "CyclicVDA.jl"))
@@ -128,30 +120,47 @@ const DEFAULT_CALLBACK = __do_nothing_callback__
 const DEFAULT_SCORE_FUNCTION = prediction_error
 
 """
-```
-fit_MVDA(algorithm, problem, ϵ, s; kwargs...)
-```
+    fit(algorithm, problem, ϵ, s; kwargs...)
 
-Solve optimization problem at sparsity levels `s` and `ϵ`-insensitive pseudodistances.
-Solution is obtained via a proximal distance `algorithm` that gradually anneals parameter estimates
+Solve optimization problem at sparsity level `s` using a deadzone of size `ϵ`.
+
+The solution is obtained via a proximal distance `algorithm` that gradually anneals parameter estimates
 toward the target sparsity set.
 """
-function fit_MVDA(algorithm, problem, ϵ::Real, s::Real; kwargs...)
-    # Initialize any additional data structures.
-    extras = __mm_init__(algorithm, problem, nothing)
-
+function fit(algorithm::AbstractMMAlg, problem::MVDAProblem, ϵ::Real, s::Real; kwargs...)
+    extras = __mm_init__(algorithm, problem, nothing) # initialize extra data structures
     fit_MVDA!(algorithm, problem, ϵ, s, extras, (true,false,); kwargs...)
 end
 
 """
-```
-fit_MVDA!(algorithm, problem, ϵ, s, [extras], [update_hyperparams]; kwargs...)
-```
+    fit!(algorithm, problem, ϵ, s, [extras], [update_extras]; kwargs...)
 
 Same as `fit_MVDA(algorithm, problem, ϵ, s)`, but with preallocated data structures in `extras`.
-The caller should specify whether to update data structures depending on `s` (default=`true`).
+
+!!! Note
+    The caller should specify whether to update data structures depending on `s` and `ρ` using `update_extras[1]` and `update_extras[2]`, respectively.
+
+    Convergence is determined based on the rule `dist < dtol || abs(dist - old) < rtol * (1 + old)`, where `dist` is the squared distance and `dtol` and `rtol` are tolerance parameters.
+
+!!! Tip
+    The `extras` argument can be constructed using `extras = __mm_init__(algorithm, problem, nothing)`.
+
+# Keyword Arguments
+
+- `nouter`: The number of outer iterations; i.e. the maximum number of `ρ` values to use in annealing (default=`100`).
+- `dtol`: An absolute tolerance parameter for the squared distance (default=`1e-6`).
+- `rtol`: A relative tolerance parameter for the squared distance (default=`1e-6`).
+- `rho_init`: The initial value for `ρ` (default=1.0).
+- `rho_max`: The maximum value for `ρ` (default=1e8).
+- `rhof`: A function `rhof(ρ, iter, rho_max)` used to determine the next value for `ρ` in the annealing sequence. The default multiplies `ρ` by `1.2`.
+- `verbose`: Print convergence information (default=`false`).
+- `cb`: A callback function for extending functionality.
+
+See also: [`anneal!`](@ref) for additional keyword arguments applied at the annealing step.
 """
-function fit_MVDA!(algorithm, problem, ϵ::Real, s::Real, extras=nothing, update_extras::NTuple{2,Bool}=(true,false,);
+function fit!(algorithm::AbstractMMAlg, problem::MVDAProblem, ϵ::Real, s::Real,
+    extras=nothing,
+    update_extras::NTuple{2,Bool}=(true,false,);
     nouter::Int=100,
     dtol::Real=1e-6,
     rtol::Real=1e-6,
@@ -160,8 +169,7 @@ function fit_MVDA!(algorithm, problem, ϵ::Real, s::Real, extras=nothing, update
     rhof::Function=DEFAULT_ANNEALING,
     verbose::Bool=false,
     cb::Function=DEFAULT_CALLBACK,
-    kwargs...
-    )
+    kwargs...)
     # Check for missing data structures.
     if extras isa Nothing
         error("Detected missing data structures for algorithm $(algorithm).")
@@ -175,8 +183,7 @@ function fit_MVDA!(algorithm, problem, ϵ::Real, s::Real, extras=nothing, update
     k = sparsity_to_k(problem, s)
 
     # Initialize ρ and iteration count.
-    ρ = rho_init
-    iters = 0
+    ρ, iters = rho_init, 0
 
     # Update data structures due to hyperparameters.
     update_extras[1] && __mm_update_sparsity__(algorithm, problem, ϵ, ρ, k, extras)
@@ -192,7 +199,7 @@ function fit_MVDA!(algorithm, problem, ϵ::Real, s::Real, extras=nothing, update
     for iter in 1:nouter
         # Solve minimization problem for fixed rho.
         verbose && print("\n",iter,"  ρ = ",ρ)
-        result = fit_MVDA!(algorithm, problem, ϵ, ρ, s, extras, (false,true,); verbose=verbose, cb=cb, kwargs...)
+        result = anneal!(algorithm, problem, ϵ, ρ, s, extras, (false,true,); verbose=verbose, cb=cb, kwargs...)
 
         # Update total iteration count.
         iters += result.iters
@@ -227,30 +234,39 @@ function fit_MVDA!(algorithm, problem, ϵ::Real, s::Real, extras=nothing, update
 end
 
 """
-```
-fit_MVDA(algorithm, problem, ϵ, ρ, s; kwargs...)
-```
+    anneal(algorithm, problem, ϵ, ρ, s; kwargs...)
 
-Solve the `ρ`-penalized optimization problem at sparsity level `s`.
+Solve the `ρ`-penalized optimization problem at sparsity level `s` with deadzone `ϵ`.
 """
-function fit_MVDA(algorithm, problem, ϵ::Real, ρ::Real, s::Real; kwargs...)
-    # Initialize any additional data structures.
+function anneal(algorithm::AbstractMMAlg, problem::MVDAProblem, ϵ::Real, ρ::Real, s::Real; kwargs...)
     extras = __mm_init__(algorithm, problem, nothing)
-
-    fit_MVDA!(algorithm, problem, ϵ, ρ, s, extras, (true,true,); kwargs...)
+    anneal!(algorithm, problem, ϵ, ρ, s, extras, (true,true,); kwargs...)
 end
 
 """
-```
-fit_MVDA!(algorithm, problem, ϵ, ρ, s, [extras], [update_extras]; kwargs...)
-The caller should specify whether to update data structures depending on
-    - `lambda` or `s` (default=`true`), and
-    - `rho` (default=true).
-```
+    anneal!(algorithm, problem, ϵ, ρ, s, [extras], [update_extras]; kwargs...)
 
-Same as `fit_MVDA!(algorithm, problem, ϵ, ρ, s)`, but with preallocated data structures in `extras`.
+Same as `anneal(algorithm, problem, ϵ, ρ, s)`, but with preallocated data structures in `extras`.
+
+!!! Note
+    The caller should specify whether to update data structures depending on `s` and `ρ` using `update_extras[1]` and `update_extras[2]`, respectively.
+
+    Convergence is determined based on the rule `gradsq < gtol`, where `gradsq` is squared Euclidean norm of the gradient and `gtol` is a tolerance parameter.
+
+!!! Tip
+    The `extras` argument can be constructed using `extras = __mm_init__(algorithm, problem, nothing)`.
+
+# Keyword Arguments
+
+- `ninner`: The maximum number of iterations (default=`10^4`).
+- `gtol`: An absoluate tolerance parameter on the squared Euclidean norm of the gradient (default=`1e-6`).
+- `nesterov_threshold`: The number of early iterations before applying Nesterov acceleration (default=`10`).
+- `verbose`: Print convergence information (default=`false`).
+- `cb`: A callback function for extending functionality.
 """
-function fit_MVDA!(algorithm, problem, ϵ::Real, ρ::Real, s::Real, extras=nothing, update_extras::NTuple{2,Bool}=(true,true);
+function anneal!(algorithm::AbstractMMAlg, problem::MVDAProblem, ϵ::Real, ρ::Real, s::Real,
+    extras=nothing,
+    update_extras::NTuple{2,Bool}=(true,true);
     ninner::Int=10^4,
     gtol::Real=1e-6,
     nesterov_threshold::Int=10,
@@ -322,9 +338,7 @@ function fit_MVDA!(algorithm, problem, ϵ::Real, ρ::Real, s::Real, extras=nothi
 end
 
 """
-```
-cv_MVDA(algorithm, problem, test_set, ϵ_grid, s_grid; kwargs...)
-```
+    cv_MVDA(algorithm, problem, test_set, ϵ_grid, s_grid; kwargs...)
 
 Compute scores for multiple models parameterized by hyperparameters `s` and `ϵ` via K-fold cross-validation.
 
@@ -333,7 +347,7 @@ Compute scores for multiple models parameterized by hyperparameters `s` and `ϵ`
 - `ϵ_grid` should be an increasing sequence of nonnegative values.
 - `s_grid` should be an increasing sequence of sparsity values (dense to sparse) between 0 and 1.
 """
-function cv_MVDA(algorithm, problem::MVDAProblem, cv_set, test_set, ϵ_grid, s_grid;
+function cv_MVDA(algorithm::AbstractMMAlg, problem::MVDAProblem, cv_set, test_set, ϵ_grid, s_grid;
     nfolds::Int=10,
     scoref::Function=DEFAULT_SCORE_FUNCTION,
     cb::Function=DEFAULT_CALLBACK,
@@ -483,132 +497,131 @@ end
 #     return result
 # end
 
-function fit_regMVDA(algorithm, problem, ϵ::Real, λ::Real; kwargs...)
-    # Initialize any additional data structures.
-    extras = __mm_init__(algorithm, problem, nothing)
+# function fit_regMVDA(algorithm, problem, ϵ::Real, λ::Real; kwargs...)
+#     # Initialize any additional data structures.
+#     extras = __mm_init__(algorithm, problem, nothing)
 
-    fit_regMVDA!(algorithm, problem, ϵ, λ, extras, true; kwargs...)
-end
+#     fit_regMVDA!(algorithm, problem, ϵ, λ, extras, true; kwargs...)
+# end
 
-function fit_regMVDA!(algorithm, problem, ϵ, λ, extras=nothing, update_extras::Bool=true;
-    ninner::Int=10^4,
-    gtol::Real=1e-6,
-    nesterov_threshold::Int=10,
-    verbose::Bool=false,
-    cb::Function=DEFAULT_CALLBACK,
-    kwargs...
-    )
-    # Check for missing data structures.
-    if extras isa Nothing
-        error("Detected missing data structures for algorithm $(algorithm).")
-    end
+# function fit_regMVDA!(algorithm, problem, ϵ, λ, extras=nothing, update_extras::Bool=true;
+#     ninner::Int=10^4,
+#     gtol::Real=1e-6,
+#     nesterov_threshold::Int=10,
+#     verbose::Bool=false,
+#     cb::Function=DEFAULT_CALLBACK,
+#     kwargs...
+#     )
+#     # Check for missing data structures.
+#     if extras isa Nothing
+#         error("Detected missing data structures for algorithm $(algorithm).")
+#     end
 
-    # Get problem info and extra data structures.
-    @unpack intercept, coeff, coeff_prev, proj = problem
+#     # Get problem info and extra data structures.
+#     @unpack intercept, coeff, coeff_prev, proj = problem
 
-    update_extras && __mm_update_lambda__(algorithm, problem, ϵ, λ, extras)
+#     update_extras && __mm_update_lambda__(algorithm, problem, ϵ, λ, extras)
 
-    # Check initial values for loss, objective, distance, and norm of gradient.
-    copyto!(proj.all, coeff.all)
-    result = __evaluate_objective_reg__(problem, ϵ, λ, extras)
-    # cb(0, problem, ϵ, 0.0, k, result)
-    old = result.objective
+#     # Check initial values for loss, objective, distance, and norm of gradient.
+#     copyto!(proj.all, coeff.all)
+#     result = __evaluate_objective_reg__(problem, ϵ, λ, extras)
+#     # cb(0, problem, ϵ, 0.0, k, result)
+#     old = result.objective
 
-    if result.gradient < gtol
-        return SubproblemResult(0, result)
-    end
+#     if result.gradient < gtol
+#         return SubproblemResult(0, result)
+#     end
 
-    # Initialize iteration counts.
-    copyto!(coeff_prev.all, coeff.all)
-    iters = 0
-    nesterov_iter = 1
+#     # Initialize iteration counts.
+#     copyto!(coeff_prev.all, coeff.all)
+#     iters = 0
+#     nesterov_iter = 1
 
-    verbose && @printf("\n%-5s\t%-8s\t%-8s\t%-8s\t%-8s", "iter.", "loss", "objective", "distance", "|gradient|²")
-    for iter in 1:ninner
-        iters += 1
+#     verbose && @printf("\n%-5s\t%-8s\t%-8s\t%-8s\t%-8s", "iter.", "loss", "objective", "distance", "|gradient|²")
+#     for iter in 1:ninner
+#         iters += 1
 
-        # Apply the algorithm map to minimize the quadratic surrogate.
-        __mm_iterate__(algorithm, problem, ϵ, λ, extras)
+#         # Apply the algorithm map to minimize the quadratic surrogate.
+#         __mm_iterate__(algorithm, problem, ϵ, λ, extras)
 
-        # Update loss, objective, distance, and gradient.
-        copyto!(proj.all, coeff.all)
-        result = __evaluate_objective_reg__(problem, ϵ, λ, extras)
+#         # Update loss, objective, distance, and gradient.
+#         copyto!(proj.all, coeff.all)
+#         result = __evaluate_objective_reg__(problem, ϵ, λ, extras)
 
-        # cb(iter, problem, ϵ, 0.0, k, result)
+#         # cb(iter, problem, ϵ, 0.0, k, result)
 
-        if verbose
-            @printf("\n%4d\t%4.3e\t%4.3e\t%4.3e\t%4.3e", iter, result.loss, result.objective, result.distance, result.gradient)
-        end
+#         if verbose
+#             @printf("\n%4d\t%4.3e\t%4.3e\t%4.3e\t%4.3e", iter, result.loss, result.objective, result.distance, result.gradient)
+#         end
 
-        # Assess convergence.
-        obj = result.objective
-        gradsq = result.gradient
-        if gradsq < gtol
-            break
-        elseif iter < ninner
-            needs_reset = iter < nesterov_threshold || obj > old
-            nesterov_iter = __apply_nesterov__!(coeff.all, coeff_prev.all, nesterov_iter, needs_reset)
-            old = obj
-        end
-    end
+#         # Assess convergence.
+#         obj = result.objective
+#         gradsq = result.gradient
+#         if gradsq < gtol
+#             break
+#         elseif iter < ninner
+#             needs_reset = iter < nesterov_threshold || obj > old
+#             nesterov_iter = __apply_nesterov__!(coeff.all, coeff_prev.all, nesterov_iter, needs_reset)
+#             old = obj
+#         end
+#     end
 
-    return  SubproblemResult(iters, result)
-end
+#     return  SubproblemResult(iters, result)
+# end
 
-function fit_MVDA(algorithm::CyclicVDA, problem, ϵ, δ, λ₁, λ₂;
-        niter::Int=10^3,
-        atol=1e-4,
-    )
-    @unpack Y, X, res, coeff = problem
-    # δ = 1 / 20
-    # ϵ = 1//2 * sqrt(2*c/(c-1))
-    n, p, c = probdims(problem)
-    μ₁ = n * λ₁
-    μ₂ = n * λ₂
+# function fit_MVDA(algorithm::CyclicVDA, problem, ϵ, δ, λ₁, λ₂;
+#         niter::Int=10^3,
+#         atol=1e-4,
+#     )
+#     @unpack Y, X, res, coeff = problem
+#     # δ = 1 / 20
+#     # ϵ = 1//2 * sqrt(2*c/(c-1))
+#     n, p, c = probdims(problem)
+#     μ₁ = n * λ₁
+#     μ₂ = n * λ₂
 
-    # initialize residuals
-    mul!(res.main.all, X, coeff.all)
-    axpby!(1.0, Y, -1.0, res.main.all)
-    extras = nothing
+#     # initialize residuals
+#     mul!(res.main.all, X, coeff.all)
+#     axpby!(1.0, Y, -1.0, res.main.all)
+#     extras = nothing
 
-    full_objective, _, _ = fetch_objective(problem, p+1, 1, ϵ, δ, λ₁, λ₂)
-    penalty1 = 0.0
-    penalty2 = 0.0
-    for j in 1:p # does not include intercept here
-        β = view(problem.coeff.all, j, :)
-        penalty1 = penalty1 + μ₁ * norm(β, 1)
-        penalty2 = penalty2 + μ₂ * norm(β, 2)
-    end
-    full_objective = full_objective + penalty2 + penalty1
+#     full_objective, _, _ = fetch_objective(problem, p+1, 1, ϵ, δ, λ₁, λ₂)
+#     penalty1 = 0.0
+#     penalty2 = 0.0
+#     for j in 1:p # does not include intercept here
+#         β = view(problem.coeff.all, j, :)
+#         penalty1 = penalty1 + μ₁ * norm(β, 1)
+#         penalty2 = penalty2 + μ₂ * norm(β, 2)
+#     end
+#     full_objective = full_objective + penalty2 + penalty1
 
-    iters = 0
-    for iter in 1:niter
-        iters += 1
+#     iters = 0
+#     for iter in 1:niter
+#         iters += 1
 
-        __mm_iterate__(algorithm, problem, ϵ, δ, λ₁, λ₂, extras)
-        loss, _, _ = fetch_objective(problem, p+1, 1, ϵ, δ, λ₁, λ₂)
-        penalty1 = 0.0
-        penalty2 = 0.0
-        for j in 1:p # does not include intercept here
-            β = view(problem.coeff.all, j, :)
-            penalty1 = penalty1 + μ₁ * norm(β, 1)
-            penalty2 = penalty2 + μ₂ * norm(β, 2)
-        end
-        objective = loss + penalty2 + penalty1
+#         __mm_iterate__(algorithm, problem, ϵ, δ, λ₁, λ₂, extras)
+#         loss, _, _ = fetch_objective(problem, p+1, 1, ϵ, δ, λ₁, λ₂)
+#         penalty1 = 0.0
+#         penalty2 = 0.0
+#         for j in 1:p # does not include intercept here
+#             β = view(problem.coeff.all, j, :)
+#             penalty1 = penalty1 + μ₁ * norm(β, 1)
+#             penalty2 = penalty2 + μ₂ * norm(β, 2)
+#         end
+#         objective = loss + penalty2 + penalty1
 
-        if objective > full_objective error("Descent failure") end
+#         if objective > full_objective error("Descent failure") end
 
-        if full_objective - objective < atol break end
-        full_objective = objective
-    end
+#         if full_objective - objective < atol break end
+#         full_objective = objective
+#     end
 
-    copyto!(problem.proj.all, coeff.all)
+#     copyto!(problem.proj.all, coeff.all)
 
-    return full_objective, penalty1, penalty2
-end
+#     return full_objective, penalty1, penalty2
+# end
 
 export IterationResult, SubproblemResult
-export MVDAProblem, SD, MMSVD, CyclicVDA
-export fit_MVDA, fit_MVDA!, cv_MVDA, fit_regMVDA, fit_regMVDA!
+export MVDAProblem, SD, MMSVD, # CyclicVDA
 
 end
--- src/algorithms/MMSVD.jl
+++ src/algorithms/MMSVD.jl
@@ -1,15 +1,15 @@
 """
-Solve least squares problem via SVD.
+Solve the least squares problem using a SVD of the design matrix.
 """
 struct MMSVD <: AbstractMMAlg end
 
 # Initialize data structures.
-function __mm_init__(::MMSVD, problem, ::Nothing)
+function __mm_init__(::MMSVD, problem::MVDAProblem, ::Nothing)
     @unpack coeff = problem
     X = get_design_matrix(problem)
     n, p, c = probdims(problem)
     T = floattype(problem)
-    nparams = ifelse(problem isa MVDAProblem, p, n)
+    nparams = ifelse(problem.kernel isa Nothing, p, n)
 
     # thin SVD of X
     U, s, V = __svd_wrapper__(X)
@@ -31,7 +31,7 @@ function __mm_init__(::MMSVD, problem, ::Nothing)
 end
 
 # Check for data structure allocations; otherwise initialize.
-function __mm_init__(::MMSVD, problem, extras)
+function __mm_init__(::MMSVD, problem::MVDAProblem, extras)
     if :projection in keys(extras) && :buffer in keys(extras) # TODO
         return extras
     else
@@ -39,11 +39,11 @@ function __mm_init__(::MMSVD, problem, extras)
     end
 end
 
-# Update data structures due to change in model subsets, k.
-__mm_update_sparsity__(::MMSVD, problem, ϵ, ρ, k, extras) = nothing
+# Update data structures due to change in model size, k.
+__mm_update_sparsity__(::MMSVD, problem::MVDAProblem, ϵ, ρ, k, extras) = nothing
 
 # Update data structures due to changing ρ.
-function __mm_update_rho__(::MMSVD, problem, ϵ, ρ, k, extras)
+function __mm_update_rho__(::MMSVD, problem::MVDAProblem, ϵ, ρ, k, extras)
     @unpack s, Ψ = extras
     n, _, _ = probdims(problem)
     a² = 1 / n
@@ -61,27 +61,27 @@ function __mm_update_rho__(::MMSVD, problem, ϵ, ρ, k, extras)
     return nothing
 end
 
-# Update data structures due to changing λ.
-function __mm_update_lambda__(::MMSVD, problem, ϵ, λ, extras)
-    @unpack s, Ψ = extras
-    n, _, _ = probdims(problem)
-    a² = 1 / n
+# # Update data structures due to changing λ.
+# function __mm_update_lambda__(::MMSVD, problem::MVDAProblem, ϵ, λ, extras)
+#     @unpack s, Ψ = extras
+#     n, _, _ = probdims(problem)
+#     a² = 1 / n
 
-    # Update the diagonal matrices Ψⱼ = (a² Σ²) / (a² Σ² + b² I).
-    for j in eachindex(Ψ)
-        Ψⱼ = Ψ[j]
-        b² = λ
-        for i in eachindex(Ψⱼ.diag)
-            sᵢ² = s[i]^2
-            Ψⱼ.diag[i] = a² * sᵢ² / (a² * sᵢ² + b²)
-        end
-    end
+#     # Update the diagonal matrices Ψⱼ = (a² Σ²) / (a² Σ² + b² I).
+#     for j in eachindex(Ψ)
+#         Ψⱼ = Ψ[j]
+#         b² = λ
+#         for i in eachindex(Ψⱼ.diag)
+#             sᵢ² = s[i]^2
+#             Ψⱼ.diag[i] = a² * sᵢ² / (a² * sᵢ² + b²)
+#         end
+#     end
 
-    return nothing
-end
+#     return nothing
+# end
 
 # Apply one update.
-function __mm_iterate__(::MMSVD, problem, ϵ, ρ, k, extras)
+function __mm_iterate__(::MMSVD, problem::MVDAProblem, ϵ, ρ, k, extras)
     @unpack intercept, coeff, proj = problem
     @unpack buffer, projection = extras
     @unpack Z, Ψ, U, s, V = extras
@@ -113,29 +113,29 @@ function __mm_iterate__(::MMSVD, problem, ϵ, ρ, k, extras)
     return nothing
 end
 
-# Apply one update in regularized version.
-function __mm_iterate__(::MMSVD, problem, ϵ, λ, extras)
-    @unpack intercept, coeff, proj = problem
-    @unpack buffer = extras
-    @unpack Z, Ψ, U, s, V = extras
-    Σ = Diagonal(s)
+# # Apply one update in regularized version.
+# function __mm_iterate__(::MMSVD, problem::MVDAProblem, ϵ, λ, extras)
+#     @unpack intercept, coeff, proj = problem
+#     @unpack buffer = extras
+#     @unpack Z, Ψ, U, s, V = extras
+#     Σ = Diagonal(s)
 
-    # need to compute Z via residuals...
-    __evaluate_residuals__(problem, ϵ, extras, true, false, true)
+#     # need to compute Z via residuals...
+#     __evaluate_residuals__(problem, ϵ, extras, true, false, true)
 
-    for j in eachindex(coeff.dim)
-        # Rename relevant arrays/views
-        βⱼ = coeff.dim[j]
-        zⱼ = view(Z, :, j)
-        Ψⱼ = Ψ[j]
+#     for j in eachindex(coeff.dim)
+#         # Rename relevant arrays/views
+#         βⱼ = coeff.dim[j]
+#         zⱼ = view(Z, :, j)
+#         Ψⱼ = Ψ[j]
 
-        # Update parameters along dimension j:
-        # βⱼ = V' * Ψⱼ * Σ⁻¹Uᵀzⱼ)
-        mul!(buffer, U', zⱼ)
-        ldiv!(Σ, buffer)
-        lmul!(Ψⱼ, buffer)
-        mul!(βⱼ, V, buffer)
-    end
+#         # Update parameters along dimension j:
+#         # βⱼ = V' * Ψⱼ * Σ⁻¹Uᵀzⱼ)
+#         mul!(buffer, U', zⱼ)
+#         ldiv!(Σ, buffer)
+#         lmul!(Ψⱼ, buffer)
+#         mul!(βⱼ, V, buffer)
+#     end
 
-    return nothing
-end
+#     return nothing
+# end
