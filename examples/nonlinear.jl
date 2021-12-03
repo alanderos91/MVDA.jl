5e5b02278375bb5d5398f29ee879689624d9c689
Alfonso Landeros <alanderos@ucla.edu>
2021-12-03 01:38:22 -0800

    Update script to reflect changes in module

--- examples/nonlinear.jl
+++ examples/nonlinear.jl
@@ -1,91 +1,104 @@
-using MLDataUtils, KernelFunctions
-include("cv-driver.jl") # load the rest
+using CSV, DataFrames, KernelFunctions, MVDA, Plots, StableRNGs
+using LinearAlgebra
 
-N = 50
-file = "/home/alanderos/Desktop/VDA/nonlinear-timings.txt"
-isfile(file) && rm(file)
-open(file, "a") do io
-    s_str = subheader("s")
-    TR_str = subheader("train")
-    V_str = subheader("validation")
-    T_str = subheader("test")
-    println(io, "replicates,dataset,sparse2dense,time,$(s_str),$(TR_str),$(V_str),$(T_str)")
-end
+BLAS.set_num_threads(8)
 
-run = function(fname, title, data, sparse2dense)
-    # extract problem info, shuffle, and create grid
-    ((n, _), _) = size(data[2]), length(unique(data[1]))
-    standardize!(data[2])
-    idx = randperm(MersenneTwister(16), n)
-    _permute!(data[1], idx)
-    _permutecols!(data[2], idx)
-    s_grid = [1-k/n for k in n:-1:0]
-    dist = zeros(binomial(n, 2))
-    K = 1
-    for i in 1:n, j in i+1:n
-        if data[1][i] != data[1][j]
-            dist[K] = norm(data[2][i,:] - data[2][j,:])
-            K += 1
-        end
-    end
-    σ = median(dist[1:K])
-    kernel = σ * RBFKernel()
+run = function(dir, example, data, sparse2dense::Bool=false; at::Real=0.8, kwargs...)
+    # Create MVDAProblem instance w/ RBFKernel and construct grids.
+    @info "Creating MVDAProblem for $(example)"
+    problem = MVDAProblem(data..., intercept=true, kernel=RBFKernel())
+    n, p, c = MVDA.probdims(problem)
+    n_cv = round(Int, n*at)
+    fill!(problem.coeff.all, 1/(n_cv+1))
+    ϵ_grid = [MVDA.maximal_deadzone(problem)]
+    s_grid = sort!([1-k/n_cv for k in n_cv:-1:0], rev=sparse2dense)
+    grids = (ϵ_grid, s_grid)
 
-    (cv_set, test_set) = splitobs(data, at=0.8, obsdim=1)
-    run_nonlinear_example(MersenneTwister(123456),
-        fname, title, kernel, cv_set, test_set, N, s_grid, sparse2dense;
-        nouter=10^2, # outer iterations
-        ninner=10^6, # inner iterations
-        nfolds=3,    # number of folds
-        gtol=1e-6,   # tolerance on gradient for convergence of inner problem
-        dtol=1e-6,   # tolerance on distance for convergence of outer problem
-        rtol=0.0,    # use strict distance criteria
+    # Collect data for cross-validation replicates.
+    @info "Cross-Validation"
+    rng = StableRNG(1903)
+    replicates = MVDA.cv_estimation(MMSVD(), problem, grids;
+        at=at,                  # propagate CV / Test split
+        rng=rng,                # random number generator for reproducibility
+        nouter=10^2,            # outer iterations
+        ninner=10^6,            # inner iterations
+        gtol=1e-6,              # tolerance on gradient for convergence of inner problem
+        dtol=1e-6,              # tolerance on distance for convergence of outer problem
+        rtol=0.0,               # use strict distance criteria
         nesterov_threshold=100, # delay on Nesterov acceleration
+        show_progress=true,     # display progress over replicates
+        kwargs...               # propagate other keywords
     )
+
+    # Write results to disk.
+    @info "Writing to disk"
+    traversal = sparse2dense ? "S2D" : "D2S"
+    partial_filename = joinpath(dir, "$(example)-NL-path=$(traversal)")
+    title = "$(example) / $(n) samples / $(p) features / $(c) classes"
+    MVDA.save_cv_results(partial_filename*".dat", replicates, grids)
+
+    # Default plot options.
+    w, h = default(:size)
+    options = (; left_margin=5Plots.mm, size=(1.5*w, 1.5*h),)
+
+    # Summarize CV results over folds + make plot.
+    @info "Summarizing over folds"
+    cv_results = CSV.read(partial_filename*".dat", DataFrame, header=true)
+    cv_paths = MVDA.cv_error(cv_results)
+    for metric in (:train, :validation, :test)
+        fig = MVDA.plot_cv_paths(cv_paths, metric)
+        plot!(fig; title=title, options...)
+        savefig(fig, partial_filename*"-replicates=$(metric).png")
+    end
+
+    # Construct credible intervals for detailed summary plot.
+    @info "Constructing credible intervals"
+    cv_intervals = MVDA.credible_intervals(cv_paths)
+    for metric in (:train, :validation, :test)
+        fig = MVDA.plot_credible_intervals(cv_intervals, metric)
+        plot!(fig; title=title, options...)
+        savefig(fig, partial_filename*"-summary=$(metric).png")
+    end
+
+    return nothing
 end
 
-# Nested Circle
-n = 200
-c = 3
-data = MVDA.generate_nested_circle(n, c; p=8//10, rng=MersenneTwister(1903))
-p = size(data[2], 2)
-example = "circles"
-title = "$(example) / $(n) samples / $(p) features / $(c) classes"
-t, s, TR, V, T = run("$(example)-NL-path=D2S", title, data, false)
-open(file, "a") do io
-    println(io, N,",",example,",",false,",",t,",",join(s,','),",",join(TR,','),",",join(V,','),",",join(T,','))
-end
+# Nested Circles
+n_cv, n_test = 250, 10^3
+nsamples = n_cv + n_test
+nclasses = 3
+data = MVDA.generate_nested_circle(nsamples, nclasses; p=8//10, rng=StableRNG(1903))
+run("/home/alanderos/Desktop/VDA/", "circles", data, false;
+    at=n_cv/nsamples,   # CV set / Test set split
+    nfolds=5,           # number of folds
+    nreplicates=100,    # number of CV replicates
+)
 
 # Waveform
-n = 300
-p = 21
-data = MVDA.generate_waveform(n, p; rng=MersenneTwister(1903))
-c = length(unique(data[1]))
-example = "waveform"
-title = "$(example) / $(n) samples / $(p) features / $(c) classes"
-t, s, TR, V, T = run("$(example)-NL-path=D2S", title, data, false)
-open(file, "a") do io
-    println(io, N,",",example,",",false,",",t,",",join(s,','),",",join(TR,','),",",join(V,','),",",join(T,','))
-end
+n_cv, n_test = 375, 10^3
+nsamples = n_cv + n_test
+nfeatures = 21
+data = MVDA.generate_waveform(nsamples, nfeatures; rng=MersenneTwister(1903))
+run("/home/alanderos/Desktop/VDA/", "waveform", data, false;
+    at=n_cv/nsamples,   # CV set / Test set split
+    nfolds=5,           # number of folds
+    nreplicates=100,    # number of CV replicates
+)
 
 # Zoo
 df = MVDA.dataset("zoo")
 data = (Vector(df[!,1]), Matrix{Float64}(df[!,2:end]))
-((n, p), c) = size(data[2]), length(unique(data[1]))
-example = "zoo"
-title = "$(example) / $(n) samples / $(p) features / $(c) classes"
-t, s, TR, V, T = run("$(example)-NL-path=D2S", title, data, false)
-open(file, "a") do io
-    println(io, N,",",example,",",false,",",t,",",join(s,','),",",join(TR,','),",",join(V,','),",",join(T,','))
-end
+run("/home/alanderos/Desktop/VDA/", "zoo", data, false;
+    at=0.8,             # CV set / Test set split
+    nfolds=5,           # number of folds
+    nreplicates=100,    # number of CV replicates
+)
 
 # Vowel
 df = MVDA.dataset("vowel")
 data = (Vector(df[!,1]), Matrix{Float64}(df[!,2:end]))
-((n, p), c) = size(data[2]), length(unique(data[1]))
-example = "vowel"
-title = "$(example) / $(n) samples / $(p) features / $(c) classes"
-t, s, TR, V, T = run("$(example)-NL-path=D2S", title, data, false)
-open(file, "a") do io
-    println(io, N,",",example,",",false,",",t,",",join(s,','),",",join(TR,','),",",join(V,','),",",join(T,','))
-end
+run("/home/alanderos/Desktop/VDA/", "vowel", data, false;
+    at=0.533333,        # CV set / Test set split
+    nfolds=5,           # number of folds
+    nreplicates=100,    # number of CV replicates
+)
