d67c88f21d21ad77c2dbcebe5c7229fbf133c8cb
Alfonso Landeros <alanderos@ucla.edu>
2021-12-06 16:48:37 -0800

    Update UCI repository examples

--- examples/uci.jl
+++ examples/uci.jl
@@ -1,53 +1,111 @@
-using MLDataUtils
-include("cv-driver.jl") # load the rest
+using CSV, DataFrames, MLDataUtils, KernelFunctions, MVDA, Plots, StableRNGs
+using LinearAlgebra, Statistics
 
-N = 50
-file = "/home/alanderos/Desktop/VDA/uci-timings.txt"
-isfile(file) && rm(file)
-open(file, "a") do io
-    s_str = subheader("s")
-    TR_str = subheader("train")
-    V_str = subheader("validation")
-    T_str = subheader("test")
-    println(io, "replicates,dataset,sparse2dense,time,$(s_str),$(TR_str),$(V_str),$(T_str)")
-end
+BLAS.set_num_threads(8)
 
-run = function(fname, title, cv_set, test_set, s_grid, sparse2dense)
-    run_example(MersenneTwister(123456), fname, title, cv_set, test_set, N, s_grid, sparse2dense;
-        nouter=10^2, # outer iterations
-        ninner=10^5, # inner iterations
-        nfolds=5,    # number of folds
-        gtol=1e-6,   # tolerance on gradient for convergence of inner problem
-        dtol=1e-6,   # tolerance on distance for convergence of outer problem
-        rtol=0.0,    # use strict distance criteria
-        nesterov_threshold=100, # delay on Nesterov acceleration
-    )
-end
-
-for example in ("iris", "lymphography", "zoo", "breast-cancer-wisconsin", "splice", "letter-recognition", "optdigits", "HAR")
-    # load the example
-    df = MVDA.dataset(example)
-    data = (Vector(df[!,1]), Matrix{Float64}(df[!,2:end]))
-
-    # purge features without variation by checking whether minimum(x) == maximum(x)
-    idx = findall(v -> v[1] != v[2], map(extrema, eachcol(data[2])))
-    data = (data[1], data[2][:, idx])
-
-    # extract problem info, shuffle, and create grid
-    ((n, p), c) = size(data[2]), length(unique(data[1]))
-    standardize!(data[2])
-    idx = randperm(MersenneTwister(16), n)
-    _permute!(data[1], idx)
-    _permutecols!(data[2], idx)
-    s_grid = [1-k/p for k in p:-1:0]
-
-    # split data for CV / testing and fit a model
-    (cv_set, test_set) = splitobs(data, at=0.8, obsdim=1)
-    filename = example
-    title = "$(example) / $(n) samples / $(p) features / $(c) classes"
-    t, s, TR, V, T = run("$(filename)-path=D2S", title, cv_set, test_set, s_grid, false) # dense -> sparse
-
-    open(file, "a") do io
-        println(io, N,",",example,",",false,",",t,",",join(s,','),",",join(TR,','),",",join(V,','),",",join(T,','))
+add_model_size_guide = function(fig, N)
+    # Compute ticks based on maximum model size N.
+    if N > 16
+        xticks = collect(N .* range(0, 1, length=11))
+    else
+        xticks = collect(0:N)
     end
+    sort!(xticks, rev=true)
+
+    # Register figure inside main subplot and append extra x-axis.
+    model_size_guide = plot(yticks=nothing, xticks=xticks, xlim=(0,N), xlabel="Model Size", xflip=true)
+    full_figure = plot(fig, model_size_guide, layout=@layout [a{1.0h}; b{1e-8h}])
+
+    return full_figure
+end
+
+run = function(dir, example, data, sparse2dense::Bool=false; at::Real=0.8, nfolds::Int=5, kwargs...)
+    # Create MVDAProblem instance w/o kernel and construct grids.
+    @info "Creating MVDAProblem for $(example)"
+    problem = MVDAProblem(data..., intercept=true, kernel=nothing)
+    n, p, c = MVDA.probdims(problem)
+    n_train = round(Int, n * at * (nfolds-1)/nfolds)
+    n_validate = round(Int, n * at * 1/nfolds)
+    n_test = round(Int, n * (1-at))
+    fill!(problem.coeff.all, 1/(p+1))
+    ϵ_grid = [MVDA.maximal_deadzone(problem)]
+    s_grid = sort!([1-k/p for k in p:-1:0], rev=sparse2dense)
+    grids = (ϵ_grid, s_grid)
+
+    @info "CV split: $(n_train) Train / $(n_validate) Validate / $(n_test) Test"
+    data_subsets = (n_train, n_validate, n_test)
+    titles = ["$(example) / $(_n) samples / $(p) features / $(c) classes" for _n in data_subsets]
+    metrics = (:train, :validation, :test)
+
+    # Collect data for cross-validation replicates.
+    rng = StableRNG(1903)
+    replicates = MVDA.cv_estimation(MMSVD(), problem, grids;
+        at=at,                  # propagate CV / Test split
+        nfolds=nfolds,          # propagate number of folds
+        rng=rng,                # random number generator for reproducibility
+        nouter=10^2,            # outer iterations
+        ninner=10^6,            # inner iterations
+        gtol=1e-6,              # tolerance on gradient for convergence of inner problem
+        dtol=1e-6,              # tolerance on distance for convergence of outer problem
+        rtol=0.0,               # use strict distance criteria
+        nesterov_threshold=100, # delay on Nesterov acceleration
+        show_progress=true,     # display progress over replicates
+        kwargs...               # propagate other keywords
+    )
+
+    # Write results to disk.
+    @info "Writing to disk"
+    traversal = sparse2dense ? "S2D" : "D2S"
+    partial_filename = joinpath(dir, "$(example)-L-path=$(traversal)")
+    MVDA.save_cv_results(partial_filename*".dat", replicates, grids)
+
+    # Default plot options.
+    w, h = default(:size)
+    options = (; left_margin=5Plots.mm, size=(1.5*w, 1.5*h),)
+
+    # Summarize CV results over folds + make plot.
+    @info "Summarizing over folds"
+    cv_results = CSV.read(partial_filename*".dat", DataFrame, header=true)
+    cv_paths = MVDA.cv_error(cv_results)
+    for (title, metric) in zip(titles, metrics)
+        fig = MVDA.plot_cv_paths(cv_paths, metric)
+        plot!(fig; title=title, options...)
+        fig = add_model_size_guide(fig, p)
+        savefig(fig, partial_filename*"-replicates=$(metric).png")
+    end
+
+    # Construct credible intervals for detailed summary plot.
+    @info "Constructing credible intervals"
+    cv_intervals = MVDA.credible_intervals(cv_paths)
+    for (title, metric) in zip(titles, metrics)
+        fig = MVDA.plot_credible_intervals(cv_intervals, metric)
+        plot!(fig; title=title, options...)
+        fig = add_model_size_guide(fig, p)
+        savefig(fig, partial_filename*"-summary=$(metric).png")
+    end
+    println()
+
+    return nothing
+end
+
+examples = (
+    ("iris", 3, 120 / 150, true),
+    ("lymphography", 3, 105 / 148, false),
+    ("zoo", 3, 0.9, false),
+    ("breast-cancer-wisconsin", 5, 0.8, false),
+    ("splice", 5, 0.8, false),
+    ("letter-recognition", 5, 0.8, false),
+    ("optdigits", 5, 3823 / 5620, false),     # use cv / test split in original dataset: 3823 + 1797
+    ("HAR", 5, 7352 / 10299, false),          # use cv / test split in original dataset: 7352 + 2947
+)
+
+for (example, nfolds, split, shuffle) in examples
+    tmp = MVDA.dataset(example)
+    df = shuffle ? shuffleobs(tmp) : tmp[:,:]
+    data = (Vector(df[!,1]), Matrix{Float64}(df[!,2:end]))
+    run("/home/alanderos/Desktop/VDA/", example, data, false;
+        at=split,           # CV set / Test set split
+        nfolds=nfolds,      # number of folds
+        nreplicates=50,     # number of CV replicates
+    )
 end
