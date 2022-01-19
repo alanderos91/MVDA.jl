c7fd03663a6bb6f7ac2f9cca5927577a339afbd8
Alfonso Landeros <alanderos@ucla.edu>
2022-01-18 16:06:23 -0800

    Add example demonstrating effect of regularization

--- examples/regularization.jl
+++ examples/regularization.jl
@@ -1,4 +1,14 @@
-using MVDA, Plots, Statistics, Random, LinearAlgebra
+using MVDA, Plots, Statistics, Random, LinearAlgebra, LaTeXStrings, StableRNGs
+
+font_main = Plots.font("Computer Modern", 12)
+font_small = Plots.font("Computer Modern", 10)
+default(
+    markerstrokewidth=0.5,
+    titlefont=font_main,
+    guidefont=font_main,
+    tickfont=font_small,
+    legendfont=font_small,
+)
 
 function draw_circle!(v, ϵ)
     xs = v[1] .+ range(-ϵ, ϵ, length=201)
@@ -7,6 +17,10 @@ function draw_circle!(v, ϵ)
     plot!(xs, ys1, color=:black, style=:dash, label="")
     plot!(xs, ys2, color=:black, style=:dash, label="")
 end
+
+# parse input arguments
+length(ARGS) < 1 && error("Script requires an input λ > 0 for regularization.")
+λ = parse(Float64, ARGS[1])
 
 # problem dimensions
 n = 300
@@ -25,44 +39,41 @@ end
 ccp = palette(:tab10)
 colors = [ccp[1] ccp[2] ccp[3]]
 ytmp = [-0.2*ones(100); 0*ones(100); 0.2*ones(100)]
-fig1 = scatter(X, ytmp; group=targets, ylims=(-0.5,0.5), yticks=nothing, color=colors, legend=:outerright, xlabel="xᵢ", title="True Classes")
+fig1 = scatter(X, ytmp; group=targets, ylims=(-0.5,0.5), yticks=nothing, color=colors, legend=:outerright, xlabel=L"x_{i}", title="True Classes vs Feature Vectors")
 
 # create a problem instance
 problem = MVDAProblem(targets, X, intercept=true)
 
-# set hyperparameters; note sparsity = 0.0 implies no model selection
-ϵ = 0.5 * sqrt(2*c/(c-1))
-sparsity = 0.0
+# set hyperparameters
+ϵ = MVDA.maximal_deadzone(problem)
 
 # compute an initial solution w/ regularization
-fill!(problem.coeff.all, 1/(p+1))
-fit_regMVDA(MMSVD(), problem, ϵ, 1.0, gtol=1e-8)
+@time MVDA.init!(SD(), problem, ϵ, λ, verbose=true, maxiter=10^4, nesterov_threshold=0, gtol=1e-6)
 
-# fit the model
-@time fit_MVDA(MMSVD(), problem, ϵ, sparsity, verbose=true, nouter=1000, ninner=10^6, rtol=0.0, gtol=1e-12, dtol=1e-6, nesterov_threshold=100);
 Y = problem.Y
 B = problem.coeff.all
-V = unique(Y, dims=1)
+V = hcat([problem.label2vertex[j] for j in 1:c]...)
+copyto!(problem.proj.all, B)
 
 # assign vertices and labels
 Yfit = problem.X*B
-labels = MVDA.classify(problem, problem.X)
+labels = MVDA.classify(problem, X)
 
 # assess misclassification error
 train_error = round(100 * (1 - sum(labels .== targets)/n), sigdigits=4)
 
 # get distances and plot with assigned labels
 dist = zeros(n, c)
-fig2 = plot(xlabel="xᵢ", ylabel="distance", ylims=(0.0, 2.25), title="Misclassification Error: $(train_error) %", legend=:bottomright)
+fig2 = plot(xlabel=L"x_{i}", ylabel="Distance to Class Vertex", ylims=(0.0, 2.25), title="Misclassification Error: $(train_error) %", legend=nothing)
 for j in 1:c
-    dist[:,j] .= [ norm(Yfit[i,:] - V[j,:]) for i in 1:n]
+    dist[:,j] .= [ norm(Yfit[i,:] - V[:,j]) for i in 1:n]
     scatter!(fig2, X, dist[:,j], label=j, color=colors[j])
 end
 hline!(fig2, [ϵ], label="", lw=3, style=:dash, color=:black)
 
 # visualize data in vertex space
-vertex_labels = map(v -> problem.vertex2label[v], eachrow(V))
-fig3 = scatter(V[:,1], V[:,2], xlabel="y₁", ylabel="y₂", title="Vertex Space", group=vertex_labels, colors=colors, legend=nothing, marker=:star, markersize=8, aspect_ratio=1)
+vertex_labels = map(v -> problem.vertex2label[v], eachcol(V))
+fig3 = scatter(V[1,:], V[2,:], xlabel=L"y_{1}", ylabel=L"y_{2}", title="Classification in Vertex Space", group=vertex_labels, colors=colors, legend=nothing, marker=:star, markersize=8, aspect_ratio=1)
 scatter!(Yfit[:,1], Yfit[:,2], group=labels, label="", color=colors, legend=:bottomright)
 foreach(v -> draw_circle!(v, ϵ), problem.vertex)
 fig3
@@ -71,4 +82,4 @@ layout = @layout [[a{0.2h}; b{0.8h}] c{0.6w}]
 w, h = default(:size)
 fig = plot(fig1, fig2, fig3, layout=layout, size=(2*w,1.1*h), margins=5Plots.mm)
 
-savefig(fig, "~/Desktop/simulated1.png")
+savefig(fig, "~/Desktop/VDA/reg-lambda=$(λ).png")
